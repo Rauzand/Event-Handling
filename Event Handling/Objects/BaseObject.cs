ecac3b43590132249266c71c2e3570df6b4cbe39
Rittey Иттер <ritey@list.ru>
2022-11-13 02:39:12 +0800

    Реализация базового объекта

--- Event Handling/Objects/BaseObject.cs
+++ Event Handling/Objects/BaseObject.cs
@@ -6,6 +6,53 @@ namespace Event_Handling.Objects
 {
     public class BaseObject
     {
-        
+        public float X;
+        public float Y;
+        public float Angle;
+
+        public Action<BaseObject, BaseObject>? OnOverlap;
+
+        public BaseObject(float x, float y, float angle)
+        {
+            X = x;
+            Y = y;
+            Angle = angle;
+        }
+
+        public virtual void Render(Graphics g)
+        {
+        }
+
+        public Matrix GetTransform()
+        {
+            var matrix = new Matrix();
+            matrix.Translate(X, Y);
+            matrix.Rotate(Angle);
+
+            return matrix;
+        }
+
+        public virtual GraphicsPath GetGraphicsPath()
+        {
+            return new GraphicsPath();
+        }
+
+        public virtual bool Overlaps(BaseObject obj, Graphics g)
+        {
+            var path1 = GetGraphicsPath();
+            var path2 = obj.GetGraphicsPath();
+
+            path1.Transform(GetTransform());
+            path2.Transform(obj.GetTransform());
+
+            var region = new Region(path1);
+            region.Intersect(path2);
+            return !region.IsEmpty(g);
+        }
+
+        public virtual void Overlap(BaseObject obj)
+        {
+            OnOverlap?.Invoke(this, obj);
+        }
     }
 }
