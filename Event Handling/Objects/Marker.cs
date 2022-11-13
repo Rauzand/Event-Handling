623b6441e856f2d37b623c85ba23ecce3a1e4ffc
Rittey Иттер <ritey@list.ru>
2022-11-13 11:36:45 +0800

    Добавление объекта маркер

---
+++ Event Handling/Objects/Marker.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Event_Handling.Objects
+{
+    public class Marker : BaseObject
+    {
+        public Marker(float x, float y, float angle) : base(x, y, angle)
+        {
+        }
+
+        public override void Render(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Red), -3, -3, 6, 6);
+            g.DrawEllipse(new Pen(Color.Red, 2), -6, -6, 12, 12);
+            g.DrawEllipse(new Pen(Color.Red, 2), -10, -10, 20, 20);
+        }
+
+        public override GraphicsPath GetGraphicsPath()
+        {
+            var path = base.GetGraphicsPath();
+            path.AddEllipse(-3, -3, 6, 6);
+            return path;
+        }
+    }
+}
